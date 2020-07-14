80cadc1b1a6266b16ef82b06b753111a8f71a8a3
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-14 08:24:16 -0500

    add exercise 2-size_me_please and change for in line 4 exercise 1

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Here we are going to create a function that calculate the shape"""
+
+
+def shape_matrix(matrix, a):
+    """Function to calculate shape right now"""
+    if type(matrix) != list:
+        return a
+    else:
+        a.append(len(matrix))
+        if type(matrix) == list and len(matrix) > 0:
+            shape_matrix(matrix[0], a)
+
+
+def matrix_shape(matrix):
+    """Functio that call another function to calculate the shape"""
+    a = []
+    for i in matrix:
+        a.append(matrix[i])
+    shape_matrix(matrix, a)
+    return a
+    
