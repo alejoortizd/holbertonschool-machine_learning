2953682b5622dc7bbd15eee9691fa734cc950e04
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-14 23:37:00 -0500

    add exercise 6-howdy_partner.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    concat = []
+    concat.extend(arr1)
+    concat.extend(arr2)
+    return concat
