b7354bcb6d6b6bf9c8aae7afe9fbf14536fca466
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-15 15:28:53 -0500

    add exercise 12-bracin_the_elements

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that return the basic operations"""
+
+
+def np_elementwise(mat1, mat2):
+    """basic operation add, sub, mul, div"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
