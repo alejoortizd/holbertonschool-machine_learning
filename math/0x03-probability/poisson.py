212b356d714586ff494e0e6cf03bc2d3e5413525
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-29 22:12:01 -0500

    first commit

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Create a class Poisson that represents a poisson distribution"""
+
+
+class Poisson:
+    """Class Poisson"""
+    def __init__(self, data=None, lambtha=1.):
+        """init class"""
+        self.lambtha = float(lambtha)
+        if data is None and self.lambtha < 0:
+            raise ValueError('lambtha must be a positive value')
+        if data is not None:
+            if type(data) != list:
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = sum(data) / len(data)
