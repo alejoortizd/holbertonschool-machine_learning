c08b0281770f10b1619500101353da2d63ee6cda
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-30 21:33:20 -0500

    add class Exponential

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -2,14 +2,16 @@
 """Create a class Exponential that represents a poisson distribution"""
 
 
-def __init__(self, data=None, lambtha=1.):
-    """init Class"""
-    self.lambtha = float(lambtha)
-    if data is None and self.lambtha <= 0:
-        raise ValueError('lambtha must be a positive value')
-    if data is not None:
-        if type(data) != list:
-            raise TypeError('data must be a list')
-        if len(data) < 2:
-            raise ValueError('data must contain multiple values')
-        self.lambtha = (sum(data)/len(data))**-1
+class Exponential:
+    """Class PoissoExponential"""
+    def __init__(self, data=None, lambtha=1.):
+        """init Class"""
+        self.lambtha = float(lambtha)
+        if data is None and self.lambtha <= 0:
+            raise ValueError('lambtha must be a positive value')
+        if data is not None:
+            if type(data) != list:
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = (sum(data)/len(data))**-1
