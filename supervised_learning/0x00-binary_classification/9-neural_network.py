a7d46cbcee180a0bb7da045b1e732012b221f380
Alejo Ortiz <davidalejandro64@gmail.com>
2020-08-08 15:54:23 -0500

    add task 9

---
+++ supervised_learning/0x00-binary_classification/9-neural_network.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""creating a neural_network class"""
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        """here start all with nx like number of input"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.ndarray((nodes, nx))
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__W2 = np.ndarray((1, nodes))
+        self.__W2[0] = np.random.normal(size=nodes)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__b2 = 0
+        self.__A1 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def A2(self):
+        return self.__A2
