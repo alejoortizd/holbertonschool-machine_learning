02617e528949419b4a90c6adb1775537f87a4d0d
Alejo Ortiz <davidalejandro64@gmail.com>
2020-08-08 15:50:10 -0500

    modified W2

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -6,8 +6,7 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """
-    class NeuralNetwork"""
+    """class NeuralNetwork"""
     def __init__(self, nx, nodes):
         """here start all with nx like number of input"""
         if type(nx) is not int:
@@ -21,6 +20,7 @@ class NeuralNetwork:
         self.W1 = np.ndarray((nodes, nx))
         self.W1 = np.random.normal(size=(nodes, nx))
         self.W2 = np.ndarray((1, nodes))
+        self.W2[0] = np.random.normal(size=nodes)
         self.b1 = np.zeros((nodes, 1))
         self.b2 = 0
         self.A1 = 0
