d2d26f0e4dac1c5c23f6954b05e2b6a5f74319d4
Alejo Ortiz <davidalejandro64@gmail.com>
2020-08-16 17:08:35 -0500

    change import of task 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     """Start the function"""
-        initializer = (tf.contrib.layers.
-                   variance_scaling_initializer(mode="FAN_AVG"))
+    initializer = (tf.contrib.layers.
+                   variance_scaling_scaling_initializer(mode="FAN_AVG"))
     return tf.layers.Dense(n, activation, name='layer',
                            kernel_initializer=initializer)(prev)
