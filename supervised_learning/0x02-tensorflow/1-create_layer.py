31fe34f61f3d2e1a943e4aeeb49fbdb5b3158e87
Alejo Ortiz <davidalejandro64@gmail.com>
2020-08-16 19:59:09 -0500

    modified line 13 erase one scaling

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -10,6 +10,6 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """Start the function"""
     initializer = (tf.contrib.layers.
-                   variance_scaling_scaling_initializer(mode="FAN_AVG"))
+                   variance_scaling_initializer(mode="FAN_AVG"))
     return tf.layers.Dense(n, activation, name='layer',
                            kernel_initializer=initializer)(prev)
